63564eb8ff0e63c4c1b989cf5c748eec45e76a4f
hotkyou <hottarakasi.stu@gmail.com>
2023-07-12 02:26:24 +0900

    add: CheckSex

--- diagnose/views.py
+++ diagnose/views.py
@@ -3,21 +3,19 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+import Application.autochecksex as checksex
+
 @csrf_exempt
 def diagnose(request):
     if request.method == 'POST':
-        # JSON文字列
+        
         datas = json.loads(request.body)
+        ACSFJ = checksex.AutoCheckFromJSON()
+        result = ACSFJ.autoCheck(datas)
+        print(result)
 
-        print("--受取り値--------------------------")
-        print(type(datas))
-        print(datas)
-
-        # requestには、param1,param2の変数がpostされたものとする
-        ret = {"data": "param1:" + datas["param1"] + ", param2:" + datas["param2"]}
-
-        # JSONに変換して戻す
-        return JsonResponse(ret)
+        return JsonResponse(result, safe=False)
     
     else:
+        
         return HttpResponse("ここをどこだと思っているんだあああ")
