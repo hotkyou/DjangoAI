db25cd2d756ae80f4d4c1fc86b77554a98f77e53
hotkyou <hottarakasi.stu@gmail.com>
2023-08-19 06:42:14 +0900

    add: ID base

--- Index/urls.py
+++ Index/urls.py
@@ -6,4 +6,7 @@ app_name = "Index"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("", views.index, name="identification-documents"),
+    path("", views.index, name="driver-license"),
+    path("", views.index, name="report-card"),
 ]
